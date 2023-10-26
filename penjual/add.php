81edd73312408f531a4e2783f44d8bb26a808383
SuperJaky <zakiramadhan120618@gmail.com>
2023-10-26 12:16:08 +0700

    crud d2

--- penjual/add.php
+++ penjual/add.php
@@ -40,7 +40,7 @@
         include_once("config.php");
                 
         // Insert user data into table
-        $result = mysqli_query($mysqli, "INSERT INTO penjual(No_Telp,email,mobile) VALUES('$name','$email','$mobile')");
+        $result = mysqli_query($mysqli, "INSERT INTO penjual(id_penjual,nama,alamat,) VALUES('$id','$nama','$alamat')");
         
         // Show message when user added
         echo "User added successfully. <a href='index.php'>View Users</a>";
